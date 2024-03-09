93dbf454c546430e36e055f6effc27b75c7f443b
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-03-09 10:29:54 +0530

    add skeleton to post

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,9 @@ import Post from "../components/Post";
 import { useLocation } from "react-router-dom";
 import { CLEARPOSTS } from "../constants/actionTypes";
 
+import { CircularProgress, Skeleton } from "@mui/material";
+import { ArrowBigDown, ArrowBigUp, MessageSquare } from "lucide-react";
+
 const Home = () => {
   const { posts, noOfPages, isLoading } = useSelector((state) => state?.posts);
 
@@ -46,18 +49,59 @@ const Home = () => {
   }, [page, noOfPages]);
 
   return (
-    <div className="flex flex-col items-center pt-28">
-      <div className="flex w-full pl-2 sm:pl-7 my-4">
-        <h1 className="font-black text-2xl text-slate-50">Your feed</h1>
-      </div>
-
-      <div className="flex flex-col sm:grid grid-cols-3 gap-4">
+    <div className="flex gap-4 justify-center pt-28 pb-40">
+      <div className="flex flex-col grid-cols-3 gap-4">
         {posts.map((post) => (
           <Post key={post?._id} post={post} />
         ))}
         {isLoading && (
-          <div className="flex justify-center items-center w-full h-20">
-            <p>Loading...</p>
+          <div className="mx-1 p-2 text-slate-800 bg-gray-50 w-[389px] rounded-xl shadow">
+            <div role="button">
+              <p className="flex justify-end">
+                <Skeleton width={40} style={{ fontSize: "12px" }} />
+              </p>
+              <div className="flex items-center gap-2">
+                <Skeleton variant="circular" width={40} height={40} />
+                <p>
+                  <Skeleton variant="text" width={80} />
+                </p>
+              </div>
+              <div className="mt-2">
+                <Skeleton variant="text" />
+              </div>
+              <div className="mt-2">
+                <Skeleton variant="text" />
+                <Skeleton variant="text" />
+              </div>
+            </div>
+            <div className="flex gap-6 text-slate-500 text-xs mt-4">
+              <div className="p-1 rounded-md  flex gap-2">
+                <button>
+                  {" "}
+                  <ArrowBigUp size={18} />
+                </button>
+
+                <p>
+                  {" "}
+                  <CircularProgress color="inherit" size={14} />
+                </p>
+
+                <button>
+                  {" "}
+                  <ArrowBigDown size={18} />
+                </button>
+              </div>
+              <div
+                role="button"
+                className="flex gap-2 items-center p-1 rounded-md"
+              >
+                <MessageSquare size={14} />
+                <p>
+                  {" "}
+                  <CircularProgress color="inherit" size={14} />
+                </p>
+              </div>
+            </div>
           </div>
         )}
       </div>
