c75d36ef1e20c7d03336d2850a60b175de02caff
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-03-18 02:47:57 +0530

    backend url added

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const app = express();
 //enable us to send post req
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors()); //enables cross origin req
+app.use(
+  cors({
+    origin: ["https://rohans-ideasloom.vercel.app"],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+); //enables cross origin req
 
 app.use("/posts", appRoutes); //app routes
 app.use("/user", userRoutes);
