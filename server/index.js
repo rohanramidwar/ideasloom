a2a8de2b1e86e40ad5fd877942834436afa1cfbb
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-03-18 03:43:15 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["https://ideasloom-gggl.vercel.app"],
+    origin: "https://ideasloom-gggl.vercel.app",
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
