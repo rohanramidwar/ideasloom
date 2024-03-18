0708fccfc92b4ae89261cb5111aaa9deca07e6a0
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-03-18 12:43:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const app = express();
 //enable us to send post req
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 app.use(
   cors({
-    origin: ["https://ideasloom-gggl.vercel.app"],
+    origin: "https://ideasloom-gggl.vercel.app",
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
