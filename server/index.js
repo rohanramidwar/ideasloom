abdaefeb9bdbbe62e77929b2b18cc53ada434c93
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-03-18 12:17:24 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 //enable us to send post req
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 app.use(
   cors({
     origin: "https://ideasloom-gggl.vercel.app",
@@ -19,6 +20,7 @@ app.use(
   })
 ); //enables cross origin req
 app.use(express.json());
+
 app.use("/posts", appRoutes); //app routes
 app.use("/user", userRoutes);
 
